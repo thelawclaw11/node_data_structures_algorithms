b43a0777259eecd8aad396d28cef5e6080fff06f
Canon Law <canon@lawcasa.com>
2022-03-09 10:37:24 -0700

    ready

--- regexTest.js
+++ regexTest.js
@@ -5,7 +5,3 @@ const alpha = "COL_18901010-19110120_001_01".match(
 const beta = "Canon".match(
     /(?<publisher>[A-Z]{3})_(?<dateRange>\d{8}-\d{8})_(?<trayIndicator>[A-Z0-9]{3})_(?<reelNumber>\d{2})/
 );
-
-console.log(beta);
-
-console.log(alpha);
